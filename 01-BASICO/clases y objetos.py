683542445821b4cf41e41209a3be0697fd73d0fd
JeO <jeo2k1@gmail.com>
2023-08-14 15:32:44 -0300

    herencia1

--- 01-BASICO/clases y objetos.py
+++ 01-BASICO/clases y objetos.py
@@ -1,9 +1,16 @@
-class Celular():
-    def __init__(self, marca, modelo, camara):
+class Celular(): # clase Celular
+    def __init__(self, marca, modelo, camara): #constructor __init__ y atributos
         self.marca = marca
         self.modelo = modelo
         self.camara = camara
+        
+    def llamar(self): # metodo llamar
+        print(f'Estas realizando una llamada desde: {self.modelo}')
+        
+    def cortar(self): # metodo cortar
+        print(f'Cortarste la llamada desde: {self.modelo}')        
     
-celular1 = Celular("Samsung","S23","48MP")
-celular2 = Celular("Apple","Iphone 15 Pro","56MP")
-print(celular2.marca)
+celular1 = Celular("Samsung","S23","48MP") # creando una instancia celular1 de la clase Celular
+celular2 = Celular("Apple","Iphone 15 Pro","56MP") # creando una instancia celular2 de la clase Celular
+
+celular1.llamar() # llamo al metodo llamar
---
+++ 02-HERENCIA/herencia.py
@@ -0,0 +1,9 @@
+class Persona:
+    def __init__(self, nombre, edad, nacionalidad):
+        self.nombre = nombre
+        self.edad = edad
+        self.nacionalidad = nacionalidad
+        
+class Empleado(Persona): #Empleado hereda de la clase Persona
+    pass
+
