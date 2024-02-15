8a348bb55ab60302946065016c6927d4c649d54e
JeO <jeo2k1@gmail.com>
2024-02-15 14:15:01 -0300

    hasta propiedades

---
+++ 03-AVANZADOS/abstraccion.py
@@ -0,0 +1,5 @@
+"""
+La abstracción es un concepto fundamental en la programación orientada a objetos (POO) que se refiere
+a la capacidad de ocultar los detalles de implementación de un objeto y enfocarse en su comportamiento.
+En Python, la abstracción se puede lograr mediante el uso de clases, métodos y herencia.
+"""
